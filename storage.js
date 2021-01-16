0cdc37d286ff0f372b84b3be037ae36f63060b03
Antoni Kepinski <xxczaki@pm.me>
2021-01-16 16:00:40 +0100

    Configure Firebase and Async Storage

---
+++ storage.js
@@ -0,0 +1,16 @@
+import storage from '@react-native-async-storage/async-storage';
+
+// If the storage is empty, supply some defaults
+(async () => {
+	const data = await storage.getItem('settings');
+
+	const defaults = {
+		isFirstRun: false
+	};
+
+	if (!data || Object.entries(JSON.parse(data)).length === 0) {
+		await storage.setItem('settings', JSON.stringify(defaults));
+	}
+})();
+
+export default storage;
